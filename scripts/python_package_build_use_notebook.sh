a57b7cdde91f22c87a4bced82a4026f01785cb07
Reza Hosseini <rhossein@rhossein-mn1.linkedin.biz>
2021-07-14 10:39:02 -0700

    minor

--- scripts/python_package_build_use_notebook.sh
+++ scripts/python_package_build_use_notebook.sh
@@ -4,8 +4,19 @@
 
 # might need: brew install openblas.
 OPENBLAS="$(brew --prefix openblas)"
-
 py_version="3.7.7"
+
+
+github_path="https://raw.githubusercontent.com/Reza1317/python_dev/main/src/"
+f1=$github_path/build_python_package.sh
+f2=$github_path/install_python_specific_version.sh
+f3=$github_path/start_python_notebook.sh
+
+
+source <(curl -s $f1)
+source <(curl -s $f2)
+source <(curl -s $f3)
+
 
 
 repo_path="$HOME/codes/getfred"
