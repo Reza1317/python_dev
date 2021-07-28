2b8aab5c25f6f881525bc892ce1d1b9c394ec919
Reza Hosseini Hosseini <rhossein@rhossein-ld2.linkedin.biz>
2021-07-28 11:33:56 -0700

    minor

--- src/os_dependencies.sh
+++ src/os_dependencies.sh
@@ -57,6 +57,7 @@ function install_dev_packages_mac() {
     sudo xcode-select --install
     # installs pip
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python3 get-pip.py
     rm -rf get-pip.py
 }
 
